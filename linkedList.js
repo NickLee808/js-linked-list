108db1067f078f65448e9323a2b4d99e424d1edc
Nick Lee <nicklee808@gmail.com>
2016-11-17 19:12:18 -1000

    add function finished

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,70 @@
  */
 function linkedListGenerator(){
 
+  var head = null;
+
+  function getHead(){
+    return head;
+  }
+
+  function getTail(){
+    let curNode = head;
+    while(curNode !== null){
+      if(curNode.next !== null){
+        curNode = curNode.next;
+      } else {
+        return curNode;
+      }
+    }
+    console.log("this is the getTail", curNode);
+    return curNode;
+  }
+
+  function add(newValue){
+    let tail = getTail();
+    let newNode = {
+      value: newValue,
+      next: null,
+    };
+    if (head === null){
+      head = newNode;
+    }else if(head === tail){
+      head.next = newNode;
+    }else if(tail.next === null){
+      tail.next = newNode;
+    }
+
+/*
+    if (gotTail !== null){
+      if (gotTail.next === null){
+        gotTail.next === }
+    }*/
+    console.log("this is the head", head);
+    console.log("this is the tail", newNode);
+    return newNode;
+  }
+
+  function remove(){
+  }
+
+  function get(){
+  }
+
+  function insert(){
+  }
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    remove: remove,
+    get: get,
+    insert: insert,
+  };
 }
+
+var ll = linkedListGenerator();
+
+var head = ll.getHead();
+
+//ll.add("cat");
